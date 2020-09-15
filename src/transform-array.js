aa9883bb42b2a9bd76dfcc61e9eb31b9ab67ec15
Petr <musakius.work@gmail.com>
2020-09-15 19:50:26 +0300

    feat: add func transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  const sequences = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev"
+  ];
+
+  let newArr = [];
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] == "--discard-next") {
+      i++;
+      continue;
+    } else if (arr[i] == "--discard-prev" && arr[i - 2] != "--discard-next") {
+      newArr.pop();
+      continue;
+    } else if (arr[i] == "--double-next") {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev" && arr[i - 2] !== "--discard-next") {
+      newArr.push(arr[i - 1]);
+    }
+
+    if (sequences.every((el) => el != arr[i])) newArr.push(arr[i]);
+  }
+
+  return newArr.filter((x) => x != undefined);
 };
